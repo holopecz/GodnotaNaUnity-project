2e7bdd6d9939cac46fb269882d983a332b8ae3c8
Kirill Ladutska <77277846+holopecz@users.noreply.github.com>
2024-03-25 16:33:24 +0100

    update.v2

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -5,27 +5,49 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     [SerializeField] Rigidbody rb;
+<<<<<<< Updated upstream
     [SerializeField] float speed , jump_height;
     private bool isGrounded;
+=======
+    [SerializeField] float speed, jump_height;
+    [HideInInspector] public bool isGrounded, doubleJump;
+
+>>>>>>> Stashed changes
     void Start()
     {
-        
+
     }
 
-  
+
     void Update()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
+<<<<<<< Updated upstream
 
         if(horizontal != 0 )
+=======
+>>>>>>> Stashed changes
         rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, horizontal * speed);
 
-        if (Input.GetKey(KeyCode.Space) && isGrounded)
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            rb.AddForce(new Vector3(0, jump_height, 0), ForceMode.Impulse);
-            isGrounded = false;
+            if (isGrounded)
+            {
+                rb.AddForce(new Vector3(0, jump_height, 0), ForceMode.Impulse);
+                isGrounded = false;
+                
+            }
+            else if (doubleJump)
+            {
+                rb.velocity = new Vector3(rb.velocity.x, 0, 0);
+                rb.AddForce(new Vector3(0, jump_height, 0), ForceMode.Impulse);
+                doubleJump = false;
+            }
+
         }
+
         rb.AddForce(new Vector3(0, -10, 0), ForceMode.Force);
+<<<<<<< Updated upstream
         Debug.Log(rb.velocity); 
     }
 
@@ -33,4 +55,11 @@ public class Movement : MonoBehaviour
     {
         isGrounded = true;
     }
+=======
+
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.up * -1, out hit, 1f))
+            isGrounded = true;
+    }
+>>>>>>> Stashed changes
 }
