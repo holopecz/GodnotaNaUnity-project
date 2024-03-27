64e11b05d8062d88923adf3501231a96c7871e38
noshuk <noshuk01@gmail.com>
2024-03-27 12:40:02 +0200

    Fade_screens
    + fade screen on lvl 1
    + fade screen on lvl 5

--- Assets/Scripts/Triggers/ChildOfConstruct.cs
+++ Assets/Scripts/Triggers/ChildOfConstruct.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ChildOfConstruct : MonoBehaviour
 {
+    [SerializeField] private Animator canvas_animr;
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.collider.CompareTag("Player"))
@@ -16,6 +18,15 @@ public class ChildOfConstruct : MonoBehaviour
             animator.SetBool("isWalking", false);
             animator.SetBool("Jump", false);
             collision.collider.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+            StartCoroutine(fade_the_screen());
         }
+    }
+
+    IEnumerator fade_the_screen()
+    {
+        yield return new WaitForSeconds(5);
+        canvas_animr.SetTrigger("roll");
+        yield return new WaitForSeconds(10);
+        SceneManager.LoadScene(0);
     }
 }
